88f334dd48c424c91d1de3bd6a910e60c8f286d8
hoooowsh <haosheng950403@gmail.com>
2024-08-04 12:23:36 -0400

    rounder clan up

--- NumPrettifierApp/NumPrettifier/NumRound.cs
+++ NumPrettifierApp/NumPrettifier/NumRound.cs
@@ -16,12 +16,13 @@ public class NumRound : NumRoundBase
 {
     public string Round(decimal num, RoundingMethod method)
     {
+        Console.WriteLine(num);
         switch (method)
         {
             case RoundingMethod.RoundUp:
-                return Math.Ceiling(num).ToString("0.#");
+                return (Math.Ceiling(num * 10) / 10).ToString("0.#");
             case RoundingMethod.RoundDown:
-                return Math.Floor(num).ToString("0.#");
+                return (Math.Floor(num * 10) / 10).ToString("0.#");
             case RoundingMethod.RoundNearest:
                 return Math.Round(num, 1).ToString("0.#");
             default:
